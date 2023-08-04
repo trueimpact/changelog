8bb054e638afdbfe2aa4d0ddc64b625d5b49927d
Hafiz Temuri <temurih@live.co.uk>
2023-08-04 10:30:37 -0600

    init

---
+++ src/app/data.tsx
@@ -0,0 +1,131 @@
+import { ChangelogProps } from '@/components/Changelog/Changelog';
+
+export const changelogs: ChangelogProps[] = [
+  {
+    date: 'July 21st, 2023',
+    logs: [
+      {
+        label: 'Fixed',
+        text: <>Fixed UX issue on ses logs page</>,
+      },
+      {
+        label: 'Improved',
+        text: <>Making impact model required</>,
+      },
+      {
+        label: 'Fixed',
+        text: <>Fixed feedback filter logic</>,
+      },
+      {
+        label: 'New',
+        text: <>Report create new program</>,
+      },
+      {
+        label: 'Improved',
+        text: (
+          <>
+            Usability issue there are few usability issues with the edit email
+            template.
+          </>
+        ),
+      },
+      {
+        label: 'Fixed',
+        text: <>Fixed forecast cursor</>,
+      },
+      {
+        label: 'New',
+        text: <>Report grid</>,
+      },
+      {
+        label: 'Fixed',
+        text: <>Feedback fixes</>,
+      },
+      {
+        label: 'New',
+        text: <>Impact receipt implementation</>,
+      },
+      {
+        label: 'Improved',
+        text: <>Help me calculate this updates</>,
+      },
+      {
+        label: 'Fixed',
+        text: (
+          <>
+            Notifications donor portfolio manager contact not working correctly
+          </>
+        ),
+      },
+      {
+        label: 'Improved',
+        text: (
+          <>Replace the static beneficiary text to dynamic beneficiary name</>
+        ),
+      },
+      {
+        label: 'Improved',
+        text: <>Add body to email logs</>,
+      },
+      {
+        label: 'Fixed',
+        text: <>Use old allocation wizard</>,
+      },
+      {
+        label: 'Improved',
+        text: <>Tasks page ui</>,
+      },
+      {
+        label: 'Improved',
+        text: <>Capacity development indicators</>,
+      },
+      {
+        label: 'Fixed',
+        text: <>Image uploading error</>,
+      },
+      {
+        label: 'Fixed',
+        text: <>Fixes description navigation</>,
+      },
+      {
+        label: 'Improved',
+        text: <>Impact details progress bar</>,
+      },
+      {
+        label: 'New',
+        text: <>Display confirmation screens</>,
+      },
+      {
+        label: 'Fixed',
+        text: <>Feedback fixes for report status</>,
+      },
+      {
+        label: 'Fixed',
+        text: <>Fixing contact emails</>,
+      },
+      {
+        label: 'Fixed',
+        text: (
+          <>
+            When user update one of the two notifications added, the language in
+            both notifications update even though just one should be updating.
+          </>
+        ),
+      },
+      {
+        label: 'Improved',
+        text: <>Status color changes</>,
+      },
+      {
+        label: 'Fixed',
+        text: <>Fixes a typo</>,
+      },
+      {
+        label: 'Improved',
+        text: (
+          <>Remove orange underline from all questions in the report builder</>
+        ),
+      },
+    ],
+  },
+];
---
+++ src/components/Changelog/Changelog.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styles from './Changelog.module.css';
+
+interface Log {
+  text: React.ReactNode;
+  label: 'New' | 'Improved' | 'Fixed';
+}
+
+const getLabelClassName = (label: Log['label']): string => {
+  switch (label) {
+    case 'New':
+      return styles.new;
+    case 'Improved':
+      return styles.improved;
+
+    default:
+      return styles.fixed;
+  }
+};
+
+export interface ChangelogProps {
+  date: string;
+  logs: Log[];
+}
+
+const Changelog: React.FC<ChangelogProps> = ({ date, logs }) => {
+  return (
+    <div className={styles.wrapper}>
+      <h2>{date}</h2>
+      {logs.map((l, i) => (
+        <div
+          key={date + '-' + i}
+          className={styles.log}
+        >
+          <div className={`${styles.label} ${getLabelClassName(l.label)}`}>
+            {l.label}
+          </div>
+          <div>{l.text}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Changelog;
