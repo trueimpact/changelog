645889a5df397deeac48dd9c928af44174e8faba
Hafiz Temuri <temurih@live.co.uk>
2023-08-04 11:44:28 -0600

    Add March 31st, 2023 Deployment

--- src/app/data.tsx
+++ src/app/data.tsx
@@ -128,4 +128,84 @@ export const changelogs: ChangelogProps[] = [
       },
     ],
   },
+  {
+    date: 'March 31st, 2023',
+    logs: [
+      {
+        label: 'Improved',
+        text: <>Ses logs mutation</>,
+      },
+      {
+        label: 'Improved',
+        text: <>Adding ses logs by email query</>,
+      },
+      {
+        label: 'Improved',
+        text: <>Notification 3 reports submitted for TI review</>,
+      },
+      {
+        label: 'Improved',
+        text: <>Solve issue updating some notification rules</>,
+      },
+      {
+        label: 'Fixed',
+        text: <>Fixes on the notifications</>,
+      },
+      {
+        label: 'Improved',
+        text: <>Notification 4 True Impact Feedback provided</>,
+      },
+
+      {
+        label: 'Fixed',
+        text: <>Merge significance True Impact stage changes into stage</>,
+      },
+      {
+        label: 'Fixed',
+        text: <>Replace significance True Impact stage changes branch</>,
+      },
+      {
+        label: 'New',
+        text: <>Donor portfolio prepare reports for export button</>,
+      },
+      {
+        label: 'New',
+        text: <>Notifications error table</>,
+      },
+      {
+        label: 'Improved',
+        text: <>Notification sends invitation notification</>,
+      },
+      {
+        label: 'Fixed',
+        text: <>v3 npo user getting internal server error upon allocation</>,
+      },
+      {
+        label: 'Fixed',
+        text: <>Notification 1 report created</>,
+      },
+      {
+        label: 'Improved',
+        text: <>Add the question_id attribute to npo report comments</>,
+      },
+      {
+        label: 'Improved',
+        text: <>Add section filter to all npo report answers</>,
+      },
+      {
+        label: 'Improved',
+        text: <>Notification 6 reminder to publish final report</>,
+      },
+      {
+        label: 'Fixed',
+        text: (
+          <>Getting server error upon allocating investment against a report</>
+        ),
+      },
+      {
+        label: 'Improved',
+        text: <>Improvements on comments and feedback</>,
+      },
+    ],
+  },
 ];
