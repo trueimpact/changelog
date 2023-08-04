124f1e4bcf581dec876708b33711531bfdab6437
Hafiz Temuri <temurih@live.co.uk>
2023-08-04 13:58:08 -0600

    adding status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Image from 'next/image';
 import styles from './page.module.css';
 import Changelog from '@/components/Changelog/Changelog';
 import { changelogs } from './data';
@@ -7,8 +8,26 @@ import { useSearchParams } from 'next/navigation';
 export default function Home() {
   const router = useSearchParams();
   const admin = router.get('admin');
+  const hideStatus = router.get('hideStatus');
   return (
     <div className={styles.main}>
+      {!Boolean(hideStatus) && (
+        <>
+          <h1>Status</h1>
+          <div className={styles.status}>
+            <Image
+              width={25}
+              height={25}
+              src="/circle-check-solid.svg"
+              alt="Checkmark"
+              title="Checkmark"
+              className={styles.icon}
+            />
+            No incidents were reported at this time. All systems are
+            operational.
+          </div>
+        </>
+      )}
       <h1>Changelogs</h1>
       <div>
         {changelogs.map((l) => (
