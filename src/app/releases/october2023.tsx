ca737b6ac38cad5088fcd4f0238a80dbfa05b711
Hafiz Temuri <temurih@live.co.uk>
2023-11-01 22:06:14 -0600

    october 30th, 2023

---
+++ src/app/releases/october2023.tsx
@@ -0,0 +1,159 @@
+import { ChangelogProps } from '@/components/Changelog/Changelog';
+
+export const october2023: ChangelogProps = {
+  date: 'October 30th, 2023',
+  logs: [
+    {
+      label: 'Improved',
+      text: <>Add Body to Email Logs.</>,
+    },
+    {
+      label: 'Fixed',
+      text: <>Notifications Donor Portfolio Manager Contact not working.</>,
+    },
+    {
+      label: 'Fixed',
+      text: <>Replace the static beneficiary text to dynamic beneficiary.</>,
+    },
+    {
+      label: 'New',
+      text: <>Impact Receipt implementation.</>,
+    },
+    {
+      label: 'Fixed',
+      text: <>Usability issues with the editor.</>,
+    },
+    {
+      label: 'New',
+      text: <>Tasks Page UI.</>,
+    },
+    {
+      label: 'Improved',
+      text: <>Use Old Allocation Wizard.</>,
+    },
+    {
+      label: 'New',
+      text: <>Capacity Development Indicators.</>,
+    },
+    {
+      label: 'New',
+      text: <>Display Confirmation Screens.</>,
+    },
+    {
+      label: 'New',
+      text: <>Impact Details Progress Bar.</>,
+    },
+    {
+      label: 'Improved',
+      text: <>Making Impact Model Required.</>,
+    },
+    {
+      label: 'Fixed',
+      text: <>Fixes Description Navigation.</>,
+    },
+    {
+      label: 'Fixed',
+      text: <>Uploading Error.</>,
+    },
+    {
+      label: 'New',
+      text: <>Report - Create New Program.</>,
+    },
+    {
+      label: 'New',
+      text: <>Add Edits Modal.</>,
+    },
+    {
+      label: 'Improved',
+      text: <>API Integration.</>,
+    },
+    {
+      label: 'New',
+      text: <>Impact Summary Editor.</>,
+    },
+    {
+      label: 'New',
+      text: (
+        <>
+          Impact Summary - Loading State, Header Update, Layout, Impact summary
+          validation.
+        </>
+      ),
+    },
+    {
+      label: 'New',
+      text: <>Report Filters.</>,
+    },
+    {
+      label: 'Improved',
+      text: <>Display questions based on whether report is allocated.</>,
+    },
+    {
+      label: 'Improved',
+      text: <>Add relevancy checkbox to budget.</>,
+    },
+    {
+      label: 'New',
+      text: <>Dates - Customize dates.</>,
+    },
+    {
+      label: 'Improved',
+      text: (
+        <>
+          Narrative Section, Beneficiary Section, Budget, Locations, Dates and
+          Period, and other associated updates.
+        </>
+      ),
+    },
+    {
+      label: 'Fixed',
+      text: <>Fixing Border Right - Email Log.</>,
+    },
+    {
+      label: 'Fixed',
+      text: <>Fixes UX issue on SES Logs Page.</>,
+    },
+    {
+      label: 'Improved',
+      text: <>Updating TimeStamp For Logs.</>,
+    },
+    {
+      label: 'Fixed',
+      text: (
+        <>
+          Tooltip, minor text updates, adding animations, spelling mistakes, and
+          other UI/UX fixes.
+        </>
+      ),
+    },
+    {
+      label: 'Fixed',
+      text: <>Fixes Completed tasks UI.</>,
+    },
+    {
+      label: 'Fixed',
+      text: <>Minor UX fix for program manager model.</>,
+    },
+    {
+      label: 'Fixed',
+      text: (
+        <>
+          Feedback fixes, including contact emails, status color changes, and
+          feedback filter logic.
+        </>
+      ),
+    },
+    {
+      label: 'Fixed',
+      text: <>Avoid duplicate indicators.</>,
+    },
+    {
+      label: 'Fixed',
+      text: <>Fix session expiry message.</>,
+    },
+    {
+      label: 'Fixed',
+      text: <>Update the YAML file for the new deployment process.</>,
+    },
+  ],
+};
